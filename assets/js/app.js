d414fd3f95d3dfb3fc3047b148fbdbd54ad46fd9
Miguel Pedraza <mikeped469@gmail.com>
2022-08-30 13:15:27 -0500

    Started projects section and added functionality to show or hide projects

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+// Variables with the div that contains the specific projects
+const frontEndProjects = document.querySelector('.frontend-projects');
+const pythonProjects = document.querySelector('.python-projects');
+const cPlusPlusProjects = document.querySelector('.cplusplus-projects');
+const swiftProjects = document.querySelector('.swift-projects');
+
+// Buttons that show the specific project
+const frontendBtn = document.getElementById('frontend-btn');
+const pythonBtn = document.getElementById('python-btn');
+const cPlusPlusBtn = document.getElementById('cplusplus-btn');
+const swiftBtn = document.getElementById('swift-btn');
+
+// Event to hide the rest of projects except frontend's
+frontendBtn.addEventListener('click', () => {
+    if (frontEndProjects.classList.contains('hide')) {
+        frontEndProjects.classList.remove('hide');
+    }
+
+    if (pythonProjects.classList.contains('hide') == false) {
+        pythonProjects.classList.add('hide');
+    }
+    if (cPlusPlusProjects.classList.contains('hide') == false) {
+        cPlusPlusProjects.classList.add('hide');
+    }
+    if (swiftProjects.classList.contains('hide') == false) {
+        swiftProjects.classList.add('hide');
+    }
+});
+
+// Event to hide the rest of projects except python's
+pythonBtn.addEventListener('click', () => {
+    if (pythonProjects.classList.contains('hide')) {
+        pythonProjects.classList.remove('hide');
+    }
+
+    if (frontEndProjects.classList.contains('hide') == false) {
+        frontEndProjects.classList.add('hide');
+    }
+    if (cPlusPlusProjects.classList.contains('hide') == false) {
+        cPlusPlusProjects.classList.add('hide');
+    }
+    if (swiftProjects.classList.contains('hide') == false) {
+        swiftProjects.classList.add('hide');
+    }
+
+});
+
+// Event to hide the rest of projects except c++'s
+cPlusPlusBtn.addEventListener('click', () => {
+    if (cPlusPlusProjects.classList.contains('hide')) {
+        cPlusPlusProjects.classList.remove('hide');
+    }
+
+    if (pythonProjects.classList.contains('hide') == false) {
+        pythonProjects.classList.add('hide');
+    }
+    if (frontEndProjects.classList.contains('hide') == false) {
+        frontEndProjects.classList.add('hide');
+    }
+    if (swiftProjects.classList.contains('hide') == false) {
+        swiftProjects.classList.add('hide');
+    }
+});
+
+// Event to hide the rest of projects except swift's
+swiftBtn.addEventListener('click', () => {
+    if (swiftProjects.classList.contains('hide')) {
+        swiftProjects.classList.remove('hide');
+    }
+
+    if (pythonProjects.classList.contains('hide') == false) {
+        pythonProjects.classList.add('hide');
+    }
+    if (cPlusPlusProjects.classList.contains('hide') == false) {
+        cPlusPlusProjects.classList.add('hide');
+    }
+    if (frontEndProjects.classList.contains('hide') == false) {
+        frontEndProjects.classList.add('hide');
+    }
+});
