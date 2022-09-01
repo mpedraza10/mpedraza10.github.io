5c3bc59a188f56b853a4255c61563862435d3778
Miguel Pedraza <mikeped469@gmail.com>
2022-09-01 09:22:58 -0500

    Finished mobile version

--- assets/js/app.js
+++ assets/js/app.js
@@ -123,3 +123,31 @@ let swiper = new Swiper('.swiper', {
     mousewheel: true,
     keyboard: true,
 });
+
+// --------------------------------------- Active link ---------------------------------------
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// --------------------------------------- Footer ---------------------------------------
+
+const year = new Date().getFullYear();
+const date = `&copy; Miguel Pedraza ${year}. All Rights Reserved.`;
+
+document.getElementsByTagName("footer")[0].innerHTML = date;
