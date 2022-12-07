16884c31a4805059715f588ec2f5ac471e880b14
Miguel Pedraza <mikeped469@gmail.com>
2022-12-07 16:10:13 -0600

    Updated transcript and resume section files

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,22 @@ const navMenu = document.getElementById("nav-menu");
 const navBtn = document.getElementById("nav-toggle");
 const closeNavBtn = document.getElementById("nav-close");
 
-if (navBtn) {
-  navBtn.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
+if (navBtn) {  
+	navBtn.addEventListener("click", () => {
+		navMenu.classList.add("show-menu");
+	});
 }
 
 if (closeNavBtn) {
-  closeNavBtn.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
+	closeNavBtn.addEventListener("click", () => {
+		navMenu.classList.remove("show-menu");
+	});
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
-  navMenu.classList.remove("show-menu");
+	navMenu.classList.remove("show-menu");
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
@@ -41,101 +41,101 @@ const swiftBtn = document.getElementById("swift-btn");
 
 // Event to hide the rest of projects except frontend's
 frontendBtn.addEventListener("click", () => {
-  frontendBtn.classList.add("project-active-btn");
-  if (frontEndProjects.classList.contains("hide")) {
-    frontEndProjects.classList.remove("hide");
-  }
+	frontendBtn.classList.add("project-active-btn");
+	if (frontEndProjects.classList.contains("hide")) {
+		frontEndProjects.classList.remove("hide");
+	}
 
-  if (pythonProjects.classList.contains("hide") == false) {
-    pythonProjects.classList.add("hide");
-    pythonBtn.classList.remove("project-active-btn");
-  }
-  if (cPlusPlusProjects.classList.contains("hide") == false) {
-    cPlusPlusProjects.classList.add("hide");
-    cPlusPlusBtn.classList.remove("project-active-btn");
-  }
-  if (swiftProjects.classList.contains("hide") == false) {
-    swiftProjects.classList.add("hide");
-    swiftBtn.classList.remove("project-active-btn");
-  }
+	if (pythonProjects.classList.contains("hide") == false) {
+		pythonProjects.classList.add("hide");
+		pythonBtn.classList.remove("project-active-btn");
+	}
+	if (cPlusPlusProjects.classList.contains("hide") == false) {
+		cPlusPlusProjects.classList.add("hide");
+		cPlusPlusBtn.classList.remove("project-active-btn");
+	}
+	if (swiftProjects.classList.contains("hide") == false) {
+		swiftProjects.classList.add("hide");
+		swiftBtn.classList.remove("project-active-btn");
+	}
 });
 
 // Event to hide the rest of projects except python's
 pythonBtn.addEventListener("click", () => {
-  pythonBtn.classList.add("project-active-btn");
-  if (pythonProjects.classList.contains("hide")) {
-    pythonProjects.classList.remove("hide");
-  }
+	pythonBtn.classList.add("project-active-btn");
+	if (pythonProjects.classList.contains("hide")) {
+		pythonProjects.classList.remove("hide");
+	}
 
-  if (frontEndProjects.classList.contains("hide") == false) {
-    frontEndProjects.classList.add("hide");
-    frontendBtn.classList.remove("project-active-btn");
-  }
-  if (cPlusPlusProjects.classList.contains("hide") == false) {
-    cPlusPlusProjects.classList.add("hide");
-    cPlusPlusBtn.classList.remove("project-active-btn");
-  }
-  if (swiftProjects.classList.contains("hide") == false) {
-    swiftProjects.classList.add("hide");
-    swiftBtn.classList.remove("project-active-btn");
-  }
+	if (frontEndProjects.classList.contains("hide") == false) {
+		frontEndProjects.classList.add("hide");
+		frontendBtn.classList.remove("project-active-btn");
+	}
+	if (cPlusPlusProjects.classList.contains("hide") == false) {
+		cPlusPlusProjects.classList.add("hide");
+		cPlusPlusBtn.classList.remove("project-active-btn");
+	}
+	if (swiftProjects.classList.contains("hide") == false) {
+		swiftProjects.classList.add("hide");
+		swiftBtn.classList.remove("project-active-btn");
+	}
 });
 
 // Event to hide the rest of projects except c++'s
 cPlusPlusBtn.addEventListener("click", () => {
-  cPlusPlusBtn.classList.add("project-active-btn");
-  if (cPlusPlusProjects.classList.contains("hide")) {
-    cPlusPlusProjects.classList.remove("hide");
-  }
+	cPlusPlusBtn.classList.add("project-active-btn");
+	if (cPlusPlusProjects.classList.contains("hide")) {
+		cPlusPlusProjects.classList.remove("hide");
+	}
 
-  if (pythonProjects.classList.contains("hide") == false) {
-    pythonProjects.classList.add("hide");
-    pythonBtn.classList.remove("project-active-btn");
-  }
-  if (frontEndProjects.classList.contains("hide") == false) {
-    frontEndProjects.classList.add("hide");
-    frontendBtn.classList.remove("project-active-btn");
-  }
-  if (swiftProjects.classList.contains("hide") == false) {
-    swiftProjects.classList.add("hide");
-    swiftBtn.classList.remove("project-active-btn");
-  }
+	if (pythonProjects.classList.contains("hide") == false) {
+		pythonProjects.classList.add("hide");
+		pythonBtn.classList.remove("project-active-btn");
+	}
+	if (frontEndProjects.classList.contains("hide") == false) {
+		frontEndProjects.classList.add("hide");
+		frontendBtn.classList.remove("project-active-btn");
+	}
+	if (swiftProjects.classList.contains("hide") == false) {
+		swiftProjects.classList.add("hide");
+		swiftBtn.classList.remove("project-active-btn");
+	}
 });
 
 // Event to hide the rest of projects except swift's
 swiftBtn.addEventListener("click", () => {
-  swiftBtn.classList.add("project-active-btn");
-  if (swiftProjects.classList.contains("hide")) {
-    swiftProjects.classList.remove("hide");
-  }
+	swiftBtn.classList.add("project-active-btn");
+	if (swiftProjects.classList.contains("hide")) {
+		swiftProjects.classList.remove("hide");
+	}
 
-  if (pythonProjects.classList.contains("hide") == false) {
-    pythonProjects.classList.add("hide");
-    pythonBtn.classList.remove("project-active-btn");
-  }
-  if (cPlusPlusProjects.classList.contains("hide") == false) {
-    cPlusPlusProjects.classList.add("hide");
-    cPlusPlusBtn.classList.remove("project-active-btn");
-  }
-  if (frontEndProjects.classList.contains("hide") == false) {
-    frontEndProjects.classList.add("hide");
-    frontendBtn.classList.remove("project-active-btn");
-  }
+	if (pythonProjects.classList.contains("hide") == false) {
+		pythonProjects.classList.add("hide");
+		pythonBtn.classList.remove("project-active-btn");
+	}
+	if (cPlusPlusProjects.classList.contains("hide") == false) {
+		cPlusPlusProjects.classList.add("hide");
+		cPlusPlusBtn.classList.remove("project-active-btn");
+	}
+	if (frontEndProjects.classList.contains("hide") == false) {
+		frontEndProjects.classList.add("hide");
+		frontendBtn.classList.remove("project-active-btn");
+	}
 });
 
 // --------------------------------------- Portfolio Swipper ---------------------------------------
 
 let swiper = new Swiper(".swiper", {
-  cssMode: true,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+	cssMode: true,
+	loop: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
 });
 
 // --------------------------------------- Active link ---------------------------------------
@@ -143,23 +143,23 @@ let swiper = new Swiper(".swiper", {
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+	const scrollY = window.pageYOffset;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector(".nav-menu a[href*=" + sectionId + "]")
+				.classList.add("active-link");
+		} else {
+			document
+				.querySelector(".nav-menu a[href*=" + sectionId + "]")
+				.classList.remove("active-link");
+		}
+	});
 }
 window.addEventListener("scroll", scrollActive);
 
@@ -167,40 +167,40 @@ window.addEventListener("scroll", scrollActive);
 
 const btn = document.getElementById("contact-submit-btn");
 
-document.getElementById("form").addEventListener("submit", function (event) {    
-    const inputName = document.getElementById('from_name');
-    const inputEmail = document.getElementById('email_id');
-    const inputMessage = document.getElementById('message');    
-    event.preventDefault();
+document.getElementById("form").addEventListener("submit", function (event) {
+	const inputName = document.getElementById("from_name");
+	const inputEmail = document.getElementById("email_id");
+	const inputMessage = document.getElementById("message");
+	event.preventDefault();
 
-    btn.value = "Sending...";
+	btn.value = "Sending...";
 
-    const serviceID = "default_service";
-    const templateID = "template_vmt3w34";
+	const serviceID = "default_service";
+	const templateID = "template_vmt3w34";
 
-    emailjs.sendForm(serviceID, templateID, this).then(
-        () => {
-        btn.value = "Send Email";        
-        Swal.fire({
-          title: 'Successful!',
-          text: 'Message was sent correctly',
-          icon: 'success',
-          confirmButtonText: 'Ok'
-        })
-        },
-        (err) => {
-        btn.value = "Send Email";        
-        Swal.fire({
-          title: 'Error!',
-          text: JSON.stringify(err),
-          icon: 'warning',
-          confirmButtonText: 'Ok'
-        })
-        }
-    );
-    inputName.value = "";
-    inputEmail.value = "";
-    inputMessage.value = "";
+	emailjs.sendForm(serviceID, templateID, this).then(
+		() => {
+			btn.value = "Send Email";
+			Swal.fire({
+				title: "Successful!",
+				text: "Message was sent correctly",
+				icon: "success",
+				confirmButtonText: "Ok",
+			});
+		},
+		(err) => {
+			btn.value = "Send Email";
+			Swal.fire({
+				title: "Error!",
+				text: JSON.stringify(err),
+				icon: "warning",
+				confirmButtonText: "Ok",
+			});
+		}
+	);
+	inputName.value = "";
+	inputEmail.value = "";
+	inputMessage.value = "";
 });
 
 // --------------------------------------- Footer ---------------------------------------
